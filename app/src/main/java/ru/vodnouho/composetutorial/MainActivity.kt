49ab36d343206f54ed4efff3ea77358b129afa91
vodnouho <ru.vodnouho@gmail.com>
2023-01-25 11:29:40 +0300

    Add Animation

--- app/src/main/java/ru/vodnouho/composetutorial/MainActivity.kt
+++ app/src/main/java/ru/vodnouho/composetutorial/MainActivity.kt
@@ -4,17 +4,24 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -26,11 +33,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTutorialTheme {
-                Surface{
-                    MessageCard(
-                        msg = Message("vodnouho", "Hello!")
-                    )
-                }
+                Conversation(messages = SampleData.conversationSample)
             }
         }
     }
@@ -39,8 +42,8 @@ class MainActivity : ComponentActivity() {
 data class Message(val author: String, val body: String)
 
 @Composable
-fun MessageCard(msg: Message){
-    Row (Modifier.padding(all = 8.dp)){
+fun MessageCard(msg: Message) {
+    Row(Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(R.drawable.profile_picture),
             contentDescription = "Contact profile picture",
@@ -49,10 +52,17 @@ fun MessageCard(msg: Message){
                 .clip(CircleShape)
                 .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
-        
+
         Spacer(modifier = Modifier.width(8.dp))
-        
-        Column {
+
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            targetValue = if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 msg.author,
                 color = MaterialTheme.colors.secondaryVariant,
@@ -60,14 +70,20 @@ fun MessageCard(msg: Message){
             )
             Spacer(modifier = Modifier.height(4.dp))
 
-            Surface (
-                    shape = MaterialTheme.shapes.medium,
-                    elevation = 1.dp
-                    ){
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
                 Text(
                     text = msg.body,
                     style = MaterialTheme.typography.body2,
                     modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                 )
             }
         }
@@ -81,9 +97,9 @@ fun MessageCard(msg: Message){
     uiMode = Configuration.UI_MODE_NIGHT_YES
 )
 @Composable
-fun PreviewMessageCard(){
+fun PreviewMessageCard() {
     ComposeTutorialTheme {
-        Surface{
+        Surface {
             MessageCard(
                 msg = Message("vodnouho", "Hello!")
             )
@@ -93,12 +109,13 @@ fun PreviewMessageCard(){
 
 @Composable
 fun Conversation(messages: List<Message>) {
-    LazyColumn { 
+    LazyColumn {
         items(messages) { message ->
-            MessageCard(msg = message)    
+            MessageCard(msg = message)
         }
     }
 }
+
 @Preview
 @Composable
 fun PreviewConversation() {
