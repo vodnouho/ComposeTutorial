ed3846469b0b1a61b4b7214e33f6a7a5027379c8
vodnouho <ru.vodnouho@gmail.com>
2023-01-25 09:51:41 +0300

    Add List Composable

--- app/src/main/java/ru/vodnouho/composetutorial/MainActivity.kt
+++ app/src/main/java/ru/vodnouho/composetutorial/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -89,3 +91,16 @@ fun PreviewMessageCard(){
     }
 }
 
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn { 
+        items(messages) { message ->
+            MessageCard(msg = message)    
+        }
+    }
+}
+@Preview
+@Composable
+fun PreviewConversation() {
+    Conversation(messages = SampleData.conversationSample)
+}
